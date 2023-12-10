41f90cd89a09c041ae56f08c3466450e97f99c4d
shetty-developer <shetty.developer@gmail.com>
2023-12-10 15:59:40 +0530

    sixth commit

--- src/Pages/PageNotFoundPage.js
+++ src/Pages/PageNotFoundPage.js
@@ -1,15 +1,10 @@
 import React from "react";
+import "../App.css";
 
 function PageNotFoundpage() {
   return (
     <div
-      style={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        padding: "10px",
-      }}
+      className="page-not-found-style"
     >
       <h2 style={{ fontSize: "40px" }}> 404 || Page Not Found Error</h2>
     </div>
